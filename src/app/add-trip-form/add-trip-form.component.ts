655f73a8c301a78b73f7fdf73e9816d4ea89090a
rahulmourya336@gmail.com <rahul@yahoo.com>
2018-10-23 23:11:30 +0530

    Add Post trip

--- src/app/add-trip-form/add-trip-form.component.ts
+++ src/app/add-trip-form/add-trip-form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 import {Router} from '@angular/router';
 import {Location} from '@angular/common';
+import {TripService} from '../trip.service';
 
 @Component({
   selector: 'app-add-trip-form',
@@ -11,9 +12,10 @@ import {Location} from '@angular/common';
 export class AddTripFormComponent implements OnInit {
   tripForm: FormGroup;
   submitted = false;
+  tripStatus = 0;
   defaultThumbnail = 'https://img.freepik.com/free-vector/travel-background-with-van-on-earth_23-2147774015.jpg?size=338&ext=jpg';
 
-  constructor(private fb: FormBuilder, private router: Router, private location: Location) {
+  constructor(private fb: FormBuilder, private router: Router, private location: Location, private Trip: TripService) {
     // check for session
     const user = localStorage.getItem('currentUser');
     if (!user) {
@@ -72,16 +74,41 @@ export class AddTripFormComponent implements OnInit {
       return;
     }
     const tripName_ = this.tripForm.value.tripName;
-    let tripImage_ = this.tripForm.value.tripImage;
-    const result = this.VerifiedUrl(tripImage_);
-     if (!this.linkStatus(tripImage_, (exists) => {})) {
-       console.log('Must be false.');
-       tripImage_ = this.defaultThumbnail;
-     }
+    // let tripImage_ = this.tripForm.value.tripImage;
+    // const result = this.VerifiedUrl(tripImage_);
+    //  if (!this.linkStatus(tripImage_, (exists) => {})) {
+    //    console.log('Must be false.');
+    //    tripImage_ = this.defaultThumbnail;
+    //  }
+    const tripImage_ = this.defaultThumbnail;
 
     const startDate_ = this.tripForm.value.startDate;
     const endDate_ = this.tripForm.value.endDate;
+    // Get current user ID
+    const credentials = JSON.parse(localStorage.getItem('currentUser'));
+    const creatorId_ = credentials.id;
 
     console.log(tripImage_, tripName_, startDate_ , endDate_);
+
+    this.Trip.createTrip(tripName_, tripImage_, startDate_, endDate_, creatorId_).subscribe(
+      data => {
+        console.log(Object.values(data) );
+        // const data_ = JSON.stringify(data);
+        if (data) {
+          if (Object.values(data)[0] === 400) {
+            this.tripStatus = 1;
+            this.tripForm.invalid;
+          } else if (Object.values(data)[0] === 201) {
+            this.router.navigate(['/dashboard']);
+          } else {
+            this.tripStatus = -1;
+            this.tripForm.invalid;
+          }
+        }
+      },
+      error => {
+        console.warn('Cannot Create trip:', error);
+      });
+
   }
 }
--- src/app/trip.service.ts
+++ src/app/trip.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createTrip(tripName: string, tripImage: string, startDate: string, endDate: string, creatorId: string) {
+    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post('http://localhost:4500/api/v1/trip/',
+      {
+        'name': tripName,
+        'url': tripImage,
+        'startDate': startDate,
+        'endDate': endDate,
+        'creatorID': creatorId,
+      }, options);
+  }
 }
